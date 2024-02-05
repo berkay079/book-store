2b83086fce8cb178d059f8f336f95b7e72996d31
berkay <berkayyildirim871@gmail.com>
2024-02-05 08:55:49 +0300

    main added comment line.

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,18 @@ import './screens/home_page.dart';
 import './screens/favori_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // Initializing the application.
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Book App',
+      title: 'Book App', // Application title.
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, // Theme color.
       ),
-      home: AppNavigator(),
+      home: AppNavigator(), // Using AppNavigator for the home page.
     );
   }
 }
@@ -25,32 +25,32 @@ class AppNavigator extends StatefulWidget {
 }
 
 class _AppNavigatorState extends State<AppNavigator> {
-  int _currentIndex = 0;
+  int _currentIndex = 0; // Initially selected page index.
 
   final List<Widget> _pages = [
-    FavoriPage(),
-    HomePage(),
+    FavoriPage(), // Favorite page.
+    HomePage(), // Home page.
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _pages[_currentIndex],
+      body: _pages[_currentIndex], // Displayed page.
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
-            _currentIndex = index;
+            _currentIndex = index; // Changing the page based on the tapped bottom navigation bar item.
           });
         },
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: 'Ana Sayfa',
+            label: 'Home', // Bottom navigation bar item - Home.
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.search),
-            label: 'Ara',
+            label: 'Search', // Bottom navigation bar item - Search.
           ),
         
         ],
